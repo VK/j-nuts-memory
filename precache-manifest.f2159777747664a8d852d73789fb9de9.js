f0b2ea94038c1d22c479f71a78b7524fe2975fa6
VK <VK@users.noreply.github.com>
2022-08-14 20:58:01 +0000

    deploy: a0091e5ce46af879b7477f8b42abaca30356349e

---
+++ precache-manifest.f2159777747664a8d852d73789fb9de9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2e4fda9e4549150fd1",
+    "url": "/css/index.d3c1899f.css"
+  },
+  {
+    "revision": "7b6ddcaf5565990505b475e935ca5c38",
+    "url": "/images/checkmark.svg"
+  },
+  {
+    "revision": "d6f3366c3b9cca5c926db886792f38da",
+    "url": "/images/cute-ghost.svg"
+  },
+  {
+    "revision": "1bc6a9eb3f93674e1076382a73841a3f",
+    "url": "/images/github-logo.svg"
+  },
+  {
+    "revision": "c7e3b9ad2afc19035cc3b2520933cc07",
+    "url": "/images/netliheart.svg"
+  },
+  {
+    "revision": "ebcba5aa7011ef57724131fb4334a223",
+    "url": "/images/play.svg"
+  },
+  {
+    "revision": "c9bab3672652b0c03075135f4b2a1cf6",
+    "url": "/images/restart.svg"
+  },
+  {
+    "revision": "9a45fc7689867939804c469bfcd17784",
+    "url": "/images/vue-logo.svg"
+  },
+  {
+    "revision": "f87e67f8f916adf71313fe55206c47c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50886fad6f2ae9d059bb",
+    "url": "/js/chunk-vendors.31ac6da3.js"
+  },
+  {
+    "revision": "5d2e4fda9e4549150fd1",
+    "url": "/js/index.a96e22da.js"
+  },
+  {
+    "revision": "85d92658f647eb97ef021caeb07c43b1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

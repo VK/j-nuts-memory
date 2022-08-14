7c9c0558e0faa2bf3e5da28145e324577549b57e
VK <VK@users.noreply.github.com>
2022-08-14 21:04:06 +0000

    deploy: 118cf1b5cb91a0bc1505ecab2f5c24aa60c6589d

---
+++ precache-manifest.c1d699c07796de8fbcd5bc2832e3165b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b171a654c018685a20",
+    "url": "/j-nuts-memory/css/index.d3c1899f.css"
+  },
+  {
+    "revision": "7b6ddcaf5565990505b475e935ca5c38",
+    "url": "/j-nuts-memory/images/checkmark.svg"
+  },
+  {
+    "revision": "d6f3366c3b9cca5c926db886792f38da",
+    "url": "/j-nuts-memory/images/cute-ghost.svg"
+  },
+  {
+    "revision": "1bc6a9eb3f93674e1076382a73841a3f",
+    "url": "/j-nuts-memory/images/github-logo.svg"
+  },
+  {
+    "revision": "c7e3b9ad2afc19035cc3b2520933cc07",
+    "url": "/j-nuts-memory/images/netliheart.svg"
+  },
+  {
+    "revision": "ebcba5aa7011ef57724131fb4334a223",
+    "url": "/j-nuts-memory/images/play.svg"
+  },
+  {
+    "revision": "c9bab3672652b0c03075135f4b2a1cf6",
+    "url": "/j-nuts-memory/images/restart.svg"
+  },
+  {
+    "revision": "9a45fc7689867939804c469bfcd17784",
+    "url": "/j-nuts-memory/images/vue-logo.svg"
+  },
+  {
+    "revision": "cee910e6ac6ccbf85d551c9711d5a295",
+    "url": "/j-nuts-memory/index.html"
+  },
+  {
+    "revision": "50886fad6f2ae9d059bb",
+    "url": "/j-nuts-memory/js/chunk-vendors.31ac6da3.js"
+  },
+  {
+    "revision": "a1b171a654c018685a20",
+    "url": "/j-nuts-memory/js/index.7ce07bcf.js"
+  },
+  {
+    "revision": "85d92658f647eb97ef021caeb07c43b1",
+    "url": "/j-nuts-memory/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/j-nuts-memory/robots.txt"
+  }
+]);
--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.f2159777747664a8d852d73789fb9de9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/j-nuts-memory/precache-manifest.c1d699c07796de8fbcd5bc2832e3165b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 

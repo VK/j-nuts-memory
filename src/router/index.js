5776e5050f1a6a550154210ee443c56de01dcc4d
VK <viktor@krueckl.de>
2022-08-20 16:53:35 +0200

    change router because of github hosting

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Game from '../views/Game.vue'
 import HighScore from '../views/HighScore.vue'
 import Levels from '../views/Levels.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
